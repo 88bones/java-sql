f6e371ecf0fed062aad8cde2a623f2a74828f7c9
88bones <123712642+88bones@users.noreply.github.com>
2023-08-08 08:48:36 +0545

    look dis

--- src/sql/readform.java
+++ src/sql/readform.java
@@ -1,8 +1,22 @@
 
 package sql;
 
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.*;
+
 
 public class readform {
+    public static void main(String[] args) {
+        int id=1;
+        try{
+            java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost/java", "root", "");
+            String sql="SELECT * FROM student";
+            PreparedStatement statement=con.prepareStatement(sql);
+            ResultSet rs=statement.execute.Query();
+            
+        }
+    }
     
     
     
